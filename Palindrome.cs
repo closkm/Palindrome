cebfe1aa442bdee5f79b402df4117b5c3cb91fd4
Michael Closky <mikeclosky29@gmail.com>
2022-07-21 10:54:00 -0400

    Add project files.

---
+++ Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Palindrome
+    {
+        private string Phrase;
+
+        public Palindrome(string phrase)
+        {
+            Phrase = phrase.ToLower();
+        }
+
+        public bool isPalindrome()
+        {
+            for(int i = 0; i < Phrase.Length; i++)
+            {
+                if (Phrase[i] == Phrase[Phrase.Length - 1 - i])
+                {
+                    continue;
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please type a word and I will see if it is a palindrome!");
+
+            string word = Console.ReadLine();
+            Palindrome palindrome = new Palindrome(word);
+            Console.WriteLine(palindrome.isPalindrome());
+        }
+    }
+}
